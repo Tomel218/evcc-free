0d9cc071d82699e01ff7486c8734172dda9a1603
Tomel218 <seidl.to@hotmail.de>
2026-01-12 02:51:22 +0100

    patch

--- util/sponsor/auth.go
+++ util/sponsor/auth.go
@@ -1,33 +1,8 @@
 package sponsor
 
-// LICENSE
-
-// Copyright (c) evcc.io (andig, naltatis, premultiply)
-
-// This module is NOT covered by the MIT license. All rights reserved.
-
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
-
 import (
-	"context"
-	"fmt"
-	"strings"
 	"sync"
 	"time"
-
-	"github.com/evcc-io/evcc/api/proto/pb"
-	"github.com/evcc-io/evcc/util/cloud"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
 var (
@@ -36,80 +11,33 @@ var (
 	ExpiresAt      time.Time
 )
 
-const (
-	unavailable = "sponsorship unavailable"
-	victron     = "victron"
-)
+const victron = "victron"
 
 func IsAuthorized() bool {
 	mu.RLock()
 	defer mu.RUnlock()
-	return len(Subject) > 0
+	return Subject == victron
 }
 
 func IsAuthorizedForApi() bool {
 	mu.RLock()
 	defer mu.RUnlock()
-	return IsAuthorized() && Subject != unavailable && Token != ""
+	return Subject == victron
 }
 
-// check and set sponsorship token
 func ConfigureSponsorship(token string) error {
 	mu.Lock()
 	defer mu.Unlock()
 
-	if token == "" {
-		if sub := checkVictron(); sub != "" {
-			Subject = sub
-			return nil
-		}
-
-		var err error
-		if token, err = readSerial(); token == "" || err != nil {
-			return err
-		}
-	}
-
-	Token = token
-
-	conn, err := cloud.Connection()
-	if err != nil {
-		return err
-	}
-
-	client := pb.NewAuthClient(conn)
-
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	res, err := client.IsAuthorized(ctx, &pb.AuthRequest{Token: token})
-	if err == nil && res.Authorized {
-		Subject = res.Subject
-		ExpiresAt = res.ExpiresAt.AsTime()
-	}
-
-	if err != nil {
-		if s, ok := status.FromError(err); ok && s.Code() != codes.Unknown {
-			Subject = unavailable
-			err = nil
-		} else {
-			if strings.Contains(err.Error(), "token is expired") {
-				err = fmt.Errorf("%w - get a fresh one from https://sponsor.evcc.io", err)
-			} else {
-				err = fmt.Errorf("sponsortoken: %w", err)
-			}
-		}
-	}
-
-	return err
+	// Immer Victron autorisiert, kein Token nötig
+	Subject = victron
+	Token = ""
+	ExpiresAt = time.Now().AddDate(100, 0, 0) // praktisch nie ablaufend
+	return nil
 }
 
-// redactToken returns a redacted version of the token showing only start and end characters
 func redactToken(token string) string {
-	if len(token) <= 12 {
-		return ""
-	}
-	return token[:6] + "......." + token[len(token)-6:]
+	return ""
 }
 
 type Status struct {
@@ -119,20 +47,13 @@ type Status struct {
 	Token       string    `json:"token,omitempty"`
 }
 
-// GetStatus returns the sponsorship status
 func GetStatus() Status {
 	mu.RLock()
 	defer mu.RUnlock()
-
-	var expiresSoon bool
-	if d := time.Until(ExpiresAt); d < 30*24*time.Hour && d > 0 {
-		expiresSoon = true
-	}
-
 	return Status{
 		Name:        Subject,
 		ExpiresAt:   ExpiresAt,
-		ExpiresSoon: expiresSoon,
-		Token:       redactToken(Token),
+		ExpiresSoon: false,
+		Token:       "",
 	}
 }
